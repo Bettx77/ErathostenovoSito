62b763eaa69c047856ef148b02d2a030a4dfb782
Bettx77 <110192243+Bettx77@users.noreply.github.com>
2024-11-06 21:15:17 +0100

    sth

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Prvocisla seznam = new Prvocisla(200);
+        Prvocisla seznam = new Prvocisla(100);
         seznam.vypis();
     }
 }
